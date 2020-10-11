29929f4dfbe7b80fde796bcd3ad5d19e06f544aa
Sobol100 <36889977+Sobol100@users.noreply.github.com>
2020-10-11 16:54:01 +0200

    Update before_script.py

--- before_script.py
+++ before_script.py
@@ -1,9 +1,19 @@
 from __future__ import print_function
 import sys
 
+
+def hello(what):
+    print('Hello, {}!'.format(what))
+
+
+def say_what():
+    return 'beforescript'
+
+
 def main():
-    print("before")
+    hello(say_what())
     return 0
-    
+
+
 if __name__ == '__main__':
-sys.exit(main())
+    sys.exit(main())
