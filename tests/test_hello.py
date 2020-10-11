a453c7f53512260ed88dc55a1edfef6a992f74a1
Sobol100 <36889977+Sobol100@users.noreply.github.com>
2020-10-11 17:10:16 +0200

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,3 +3,15 @@ import hello
 
 def test_says_world():
     assert hello.say_what() == 'world'
+
+def test_says_world1():
+    assert 1 == 1
+    
+def test_says_world2():
+    assert 4 == 4
+    
+def test_says_world3():
+    assert 3 == 3
+    
+def test_says_world4():
+    assert 2 == 2
